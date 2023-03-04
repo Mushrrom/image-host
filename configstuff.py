989f7fadb0c37bef0c40e14afc53c0cbb5cf008d
Mushrrom <94596520+Mushrrom@users.noreply.github.com>
2023-03-04 17:59:21 +1100

    I am an idiot but it works now yay

--- configstuff.py
+++ configstuff.py
@@ -6,7 +6,7 @@ import os
 # Why tf is my IDE trying to teach me grammar leave me alone please
 
 def configsutff():  # TyPo iN wORd cOnfIgStUfF
-    os.environ["images_path"] = "images"
+    os.environ["images_path"] = "./images"
     os.environ["main_path"] = "."  # wsgi cant do local reading. And I am not manually setting this in each file
     os.environ["URL"] = "127.0.0.1:5000"
     os.environ["data_path"] = "./data"
---
+++ createaccount.py
@@ -0,0 +1,52 @@
+import os
+import sys
+import configstuff
+import random
+import json
+import time
+# This is for creating accounts on the server idk if I will create a better way to do this
+configstuff.configsutff()
+
+ln = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+data_path = os.environ.get("data_path")
+images_path = os.environ.get("images_path")
+print(data_path)
+
+token = sys.argv[1]
+
+if not os.path.exists(f"{data_path}/users"):
+    print("creating users directory")
+    os.mkdir(f"{data_path}/users")
+
+username = ""
+
+if len(sys.argv) == 2:
+    i = 0
+    while i == 0:
+        username = f"{ln[random.randint(0, 61)]}{ln[random.randint(0, 61)]}"
+        if not os.path.exists(f"{data_path}/users/{username}"):
+            os.mkdir(f"{data_path}/users/{username}")
+            os.mkdir(f"{data_path}/users/{username}/images")
+            os.mkdir(f"{images_path}/{username}")
+            with open(f"{data_path}/users/{username}/user.json", "w") as f:
+                data = {"token": token, "storage_used": 0, "uploads": 0, "public_name": "example",
+                        "creationdate": time.time()}
+                json.dump(data, f)
+            i = 1
+else:
+    username = sys.argv[2]
+    if not os.path.exists(f"{data_path}/users/{username}"):
+        os.mkdir(f"{data_path}/users/{username}")
+        os.mkdir(f"{data_path}/users/{username}/images")
+        os.mkdir(f"{images_path}/{username}")
+        with open(f"{data_path}/users/{username}/user.json", "w") as f:
+            data = {"token": token, "storage_used": 0, "uploads": 0, "public_name": "example", "creationdate": time.time()}
+            json.dump(data, f)
+    else:
+        print("Username already used")
+        quit()
+
+
+print(f"created account with username: {username} and token: {token}")
+
